2de376bb4a1986b3016e22d7daf4038bcae04f9f
erezushi <erezushi900@gmail.com>
2025-01-27 15:20:05 +0200

    Fixed navbar server-render error

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -5,55 +5,58 @@ import { Button } from '@mui/material';
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Suspense, useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const Navbar = () => {
-  if (typeof window === 'undefined') {
-    throw Error('NavBar should only render on the client.');
-  }
   const [isMenuCollapsed, setIsMenuCollapsed] = useState(true);
 
   const toggleMenuCollapse = useCallback(() => {
     setIsMenuCollapsed((oldValue) => !oldValue);
   }, []);
 
-  const mediaQuery = window.matchMedia('(max-width: 465px)');
+  let mediaQuery: MediaQueryList | { matches: boolean };
+
+  try {
+    mediaQuery = window.matchMedia('(max-width: 465px)');
+  } catch (error) {
+    mediaQuery = { matches: false };
+  }
 
   useEffect(() => {
     setIsMenuCollapsed(mediaQuery.matches);
   }, []);
 
   return (
-    <Suspense fallback={<></>}>
-      <div
-        className={clsx({
-          'navbar-container': true,
-          collapsed: isMenuCollapsed,
-        })}
-      >
-        {mediaQuery.matches && (
-          <Button
-            onClick={toggleMenuCollapse}
-            className="navbar-collapse-button"
-          >
-            <MenuRounded htmlColor={isMenuCollapsed ? 'black' : 'white'} />
-          </Button>
-        )}
-        <div className="navbar">
-          <Link href="/">
-            <Image
-              className="site-logo"
-              src="/PokéErez Logo.webp"
-              height={50}
-              width={50}
-              alt="PokéErez channel logo"
+    <div
+      className={clsx({
+        'navbar-container': true,
+        collapsed: isMenuCollapsed,
+      })}
+    >
+      {mediaQuery.matches && (
+        <Button onClick={toggleMenuCollapse} className="navbar-collapse-button">
+          <MenuRounded htmlColor={isMenuCollapsed ? 'black' : 'white'} />
+        </Button>
+      )}
+      <div className="navbar">
+        <Link href="/">
+          <Image
+            className="site-logo"
+            src="/PokéErez Logo.webp"
+            height={50}
+            width={50}
+            alt="PokéErez channel logo"
+            priority
+          />
+        </Link>
+        <Link href="YouTube">YouTube</Link>
               priority
             />
           </Link>
           <Link href="YouTube">YouTube</Link>
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
