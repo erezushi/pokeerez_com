c42b9fe3a7208cef093a556e734064a9e1a5d3f8
erezushi <erezushi900@gmail.com>
2025-01-30 19:14:42 +0200

    Fixed punctuation consistency and added Linktree link

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,18 @@ const Home = () => {
       With my brother, I also run a non-Pokémon focused channel called{' '}
       <Link href="https://youtube.com/@erorbros">ErOr Bros.</Link>, which also has its own{' '}
       <Link href="https://twitter.com/erorbros">Twitter</Link> and{' '}
-      <Link href="https://erorbros.bsky.social">Bluesky</Link>
+      <Link href="https://erorbros.bsky.social">Bluesky</Link>.
       <br />
       <br />
       Other than my content creation endeavors, I also dabble in programming.
       <br />
       Not only is this website completely coded by me, but I also have other websites: a{' '}
       <Link href="https://generator.pokeerez.com">random Pokémon generator tool</Link> and what I
-      like to call <Link href="https://names.pokeerez.com">The Name Game</Link>
+      like to call <Link href="https://names.pokeerez.com">The Name Game</Link>.
       <br />
       <br />
-      Browse the different pages for further details on each section
+      Browse the different pages for further details on each section, or check out my{' '}
+      <Link href="https://linktr.ee/erezushi">Linktree</Link> if you only want to find my links.
     </div>
   );
 };
