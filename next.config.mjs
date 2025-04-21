31316b2cc79a82ab66a2510184c930b7c83f508d
erezushi <erezushi900@gmail.com>
2025-04-21 16:31:38 +0300

    Added alternative origin IP

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.1.*'],
+  allowedDevOrigins: ['192.168.1.*', '172.16.0.*'],
 };
 
 export default nextConfig;
