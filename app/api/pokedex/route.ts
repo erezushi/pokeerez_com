7d022e1399ffc5a1e53c299047e3bcb169891086
PokéErez <erezushi900@gmail.com>
2026-07-06 20:44:54 +0300

    Added Weakness option to Pokédex route

--- app/api/pokedex/route.ts
+++ app/api/pokedex/route.ts
@@ -1,7 +1,8 @@
-import Pokedex from 'pokedex-promise-v2';
+import { getTypes } from '@erezushi/pokemon-randomizer';
 import { capitalize, isArray, startCase } from 'lodash';
-import { chainFormatter, pokedexGames, vowels } from './_utils';
 import { NextRequest } from 'next/server';
+import Pokedex from 'pokedex-promise-v2';
+import { chainFormatter, pokedexGames, vowels } from './_utils';
 
 const pokedex = new Pokedex();
 
@@ -80,8 +81,66 @@ export const GET = async (request: NextRequest) => {
             .join(', ')
             .replace(/,([^,]*)$/, ' and$1')}.`,
         );
+      } else if (info === 'weakness') {
+        const typeList = getTypes();
+        const multipliers = Object.fromEntries(
+          Object.entries(typeList).map(([typeName]) => [typeName, 1]),
+        );
+
+        apiPokemon.types.forEach((typeObj) => {
+          const typeName = typeObj.type.name as keyof typeof typeList;
+          const { vulnerable, resists, immune } = typeList[typeName];
+
+          if (vulnerable !== '') {
+            vulnerable.split(' ').forEach((vulType) => {
+              multipliers[vulType] *= 2;
+            });
+          }
+
+          if (resists !== '') {
+            resists.split(' ').forEach((resType) => {
+              multipliers[resType] /= 2;
+            });
+          }
+
+          if (immune !== '') {
+            immune.split(' ').forEach((immType) => {
+              multipliers[immType] = 0;
+            });
+          }
+        });
+
+        const results = {
+          weakTo: Object.entries(multipliers)
+            .filter(([_, multiplier]) => multiplier > 1)
+            .map(([type]) => capitalize(type)),
+          resists: Object.entries(multipliers)
+            .filter(([_, multiplier]) => multiplier < 1 && multiplier !== 0)
+            .map(([type]) => capitalize(type)),
+          immuneTo: Object.entries(multipliers)
+            .filter(([_, multiplier]) => multiplier === 0)
+            .map(([type]) => capitalize(type)),
+        };
+
+        return new Response(
+          `Ignoring abilities and special conditions, ${pokemonName} is weak to ${results.weakTo
+            .join(', ')
+            .replace(/,([^,]*)$/, ' and$1')}, ${
+            results.resists.length
+              ? `it resists ${results.resists.join(', ').replace(/,([^,]*)$/, ' and$1')}, `
+              : ''
+          }${
+            results.immuneTo.length
+              ? `it's immune to ${results.immuneTo.join(', ').replace(/,([^,]*)$/, ' and$1')}, `
+              : ''
+          }`
+            .replace(/, $/, '.')
+            .replace(/, it(?!.*, it)/, ' and it'),
+        );
+
+        // return new Response(JSON.stringify(multipliers, null, 2));
       } else {
-        return new Response('Info can only be one of: generic, evolution, numbers');
+        return new Response('Info can only be one of: generic, evolution, numbers, weakness');
       }
     } catch (error) {
       return new Response(
@@ -91,7 +150,7 @@ export const GET = async (request: NextRequest) => {
   } else {
     return new Response(`Syntax: !pokedex [pokemon] [info] [form].
         pokemon: Pokémon name or natDex number.
-        info: generic/evolution/numbers.
+        info: generic/evolution/numbers/weakness.
         form: Pokémon form, use 'default' for regular/no form.`);
   }
 };
